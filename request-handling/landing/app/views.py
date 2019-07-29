138c4f2c3ccfd8b5e87feae32c068be57dd3f953
Undress <andrejs.proskurins@gmail.com>
2019-07-30 00:27:21 +0300

    Задание сделано

--- request-handling/landing/app/views.py
+++ request-handling/landing/app/views.py
@@ -12,6 +12,11 @@ counter_click = Counter()
 
 def index(request):
     # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
+    req_param = request.GET.get('from-landing', 'none')
+    if req_param == 'original':
+        counter_click['original'] += 1
+    elif req_param == 'test':
+        counter_click['test'] += 1
     return render_to_response('index.html')
 
 
@@ -20,7 +25,16 @@ def landing(request):
     # в зависимости от GET параметра ab-test-arg
     # который может принимать значения original и test
     # Так же реализуйте логику подсчета количества показов
-    return render_to_response('landing.html')
+    req_param = request.GET.get('ab-test-arg', 'none')
+    if req_param == 'original':
+        counter_show['original'] += 1
+        return render_to_response('landing.html')
+    elif req_param == 'test':
+        counter_show['test'] += 1
+        return render_to_response('landing_alternate.html')
+    else:
+        return render_to_response('landing.html')
+
 
 
 def stats(request):
@@ -28,7 +42,17 @@ def stats(request):
     # Чтобы отличить с какой версии лендинга был переход
     # проверяйте GET параметр marker который может принимать значения test и original
     # Для вывода результат передайте в следующем формате:
+    original_conversion = 0
+    test_conversion = 0
+
+    print (counter_show, counter_click)
+    if counter_show['original'] > 0:
+        original_conversion = round(counter_click['original'] / counter_show['original'], 2)
+    if counter_show['test'] > 0:
+        test_conversion = round(counter_click['test'] / counter_show['test'], 2)
+
+
     return render_to_response('stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+        'test_conversion': test_conversion,
+        'original_conversion': original_conversion,
     })
