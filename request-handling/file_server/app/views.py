3a6271f6f4c55166d560deed8a6d29fe9f9f0a35
Undress <andrejs.proskurins@gmail.com>
2019-08-05 16:19:55 +0300

    доработка

--- request-handling/file_server/app/views.py
+++ request-handling/file_server/app/views.py
@@ -26,29 +26,40 @@ def file_list(request, date=None):
     dir_list = os.listdir(settings.FILES_PATH)
 
     context['files'] = []
+
+    if date:
+        context['date'] = datetime.datetime.strptime(date, '%Y-%m-%d')
+        print(context['date'])
+
     for file in dir_list:
         file_path = f'{settings.FILES_PATH}/{file}' 
         file_data = os.stat(file_path)
         file_ctime = datetime.datetime.fromtimestamp(file_data.st_ctime)
         file_mtime = datetime.datetime.fromtimestamp(file_data.st_mtime)
+        
+        if not(date):
+            context['files'].append({'name' : file, 'ctime' : file_ctime, 'mtime' : file_mtime})
+        elif context['date'].date() == file_ctime.date() or context['date'].date() == file_mtime.date():
+            context['files'].append({'name' : file, 'ctime' : file_ctime, 'mtime' : file_mtime})
 
-        context['files'].append({'name' : file, 'ctime' : file_ctime, 'mtime' : file_mtime})
 
-    if date:
-        context['date'] = date
 
     return render(request, template_name, context)
 
 
 def file_content(request, name):
     # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
-    file_path = f'{settings.FILES_PATH}/{name}'
-    f = open(file_path, "r")
-    content = f.read()
-    
+    file_path = os.path.join(settings.FILES_PATH, name)
+    if os.path.exists(file_path):
+        f = open(file_path, "r")
+        content = f.read()
+
+    else:
+        content = "Указанный файл не существует"
+
     return render(
         request,
         'file_content.html',
-        context={'file_name': 'file_name_1.txt', 'file_content': content}
+        context={'file_name': name, 'file_content': content}
     )
 
