104a0b10786d58eb9f5f9d2f423fd14e8fed18cd
Undress <andrejs.proskurins@gmail.com>
2019-08-05 16:50:29 +0300

    доработка

--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render_to_response, redirect
 from django.urls import reverse
 from django.conf import settings
 from django.http import HttpRequest
-
+from django.core.paginator import Paginator
 
 
 def index(request):
@@ -18,40 +18,36 @@ def bus_stations(request):
 	context = {}
 	context['bus_stations'] = []
 	current_page_num = 1
-	prev_page_num = None
-	next_page_num = 2
-	file_length = 0
-	last_row = 0
 
 
 	if request.GET.get('page'):
 		current_page_num = int(request.GET.get('page'))
-		next_page_num = current_page_num + 1
-		prev_page_num = current_page_num - 1
+
 	
 	context['current_page'] = current_page_num	
-
-	if prev_page_num == 0 or prev_page_num == None: 
-		context['prev_page_url'] = None 
-	else:
-		context['prev_page_url'] = reverse(bus_stations) + '?' + urllib.parse.urlencode({'page' : prev_page_num})
-	
 
 	with open(settings.BUS_STATION_CSV, 'r', encoding='cp1251') as csvfile:
 
 		file_content = list(csv.DictReader(csvfile))
 		file_length = len(file_content)
 
-		for row in itertools.islice(file_content, (current_page_num - 1) * 10, (next_page_num - 1) * 10):
+		paginator = Paginator(file_content, 10)
+
+		page = paginator.get_page(current_page_num)
+
+		for row in page.object_list:
 			context['bus_stations'].append({'Name': row['Name'], 'Street' : row['Street'], 'District' : row['District']})
-			last_row = file_content.index(row)
 
 
-	if last_row == file_length - 1:
+	if page.has_next():
+		context['next_page_url'] = reverse(bus_stations) + '?' + urllib.parse.urlencode({'page' : page.next_page_number()})
+	else: 
 		context['next_page_url'] = None
-	else:
-		context['next_page_url'] = reverse(bus_stations) + '?' + urllib.parse.urlencode({'page' : next_page_num})
 
+	if page.has_previous():
+		context['prev_page_url'] = reverse(bus_stations) + '?' + urllib.parse.urlencode({'page' : page.previous_page_number()})
+	else:
+		context['prev_page_url'] = None 
 
 
 	return render_to_response('index.html', context = context)
