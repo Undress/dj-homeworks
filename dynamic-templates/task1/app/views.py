78b065ad88ed3ffe7135823930fda27ca3ce891d
Undress <andrejs.proskurins@gmail.com>
2019-08-06 23:05:30 +0300

    Задание готово

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -1,3 +1,4 @@
+import csv
 from django.shortcuts import render
 
 def inflation_view(request):
@@ -5,6 +6,26 @@ def inflation_view(request):
 
     # чтение csv-файла и заполнение контекста
     context = {}
+    context['data'] = []
+    data_fields = []
 
+    with open ('inflation_russia.csv', newline='') as csvfile:
+    	reader = list(csv.reader(csvfile, delimiter=';'))
+    	for row in reader:
+    		if reader.index(row) == 0:
+    			context['header'] = row
+    		else:
+    			for index, item in enumerate(row):
+    				try:
+    					if index == 0:
+    						row[index] = item	
+    					else:
+    						row[index] = float(item)
+    				except ValueError:
+    					row[index] = item 
+    			context['data'].append(row)
+    			# context[row[0]] = row[1:]
+    		
+    print(context)
     return render(request, template_name,
                   context)
