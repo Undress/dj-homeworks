3377db88ca3259a8f1912e03b666eea3128b4e0d
Undress <andrejs.proskurins@gmail.com>
2019-08-14 13:58:16 +0300

    Ошибки исправлены

--- dynamic-templates/task3/app/templatetags/news_filters.py
+++ dynamic-templates/task3/app/templatetags/news_filters.py
@@ -15,7 +15,7 @@ def format_date(value):
 		result = datetime.date.fromtimestamp(value).isoformat()
 	elif (time_diff.seconds / 60) < 10:
 		result = 'только что'
-	elif (time_diff.seconds / 60) > 10 and (time_diff.seconds / 60) < 1440:
+	elif (time_diff.seconds / 60) >= 10 and (time_diff.seconds / 60) < 1440:
 		result = f'{round(time_diff.seconds / 3600)} часов назад'
 		
 
@@ -28,9 +28,9 @@ def format_score(value):
 
 	if value < -5:
 		return "все плохо"
-	elif value > -5 and value < 5:
+	elif value >= -5 and value < 5:
 		return "нейтрально"
-	elif value > 5:
+	elif value >= 5:
 		return "хорошо"
 	else:
 		return value
@@ -40,9 +40,9 @@ def format_num_comments(value):
     
     if value == 0:
     	return "Оставьте комментарий"
-    elif value > 0 and value < 50:
+    elif value >= 0 and value < 50:
     	return value
-    elif value > 50:
+    elif value >= 50:
     	return "50+"
 
 
@@ -51,9 +51,12 @@ def format_selfText(value, args):
 
 	result_list = []
 	text_list = value.split(' ')
-	result_list.append(' '.join(text_list[:args]))
-	result_list.append('...')
-	result_list.append(' '.join(text_list[-args:]))
-	result = ' '.join(result_list)
+	if len(text_list) > 2 * args:
+		result_list.append(' '.join(text_list[:args]))
+		result_list.append('...')
+		result_list.append(' '.join(text_list[-args:]))
+		result = ' '.join(result_list)
+	else:
+		result = value
 
 	return result
