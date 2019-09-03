7c618ae1c90e5d6d9296e17f0ccdd6c48a6e349e
Undress <andrejs.proskurins@gmail.com>
2019-09-03 23:03:08 +0300

    Задание выполнено

--- databases/work_with_database/phones/models.py
+++ databases/work_with_database/phones/models.py
@@ -3,4 +3,11 @@ from django.db import models
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    id = models.AutoField(primary_key=True)
+    name = models.CharField('name', max_length=64)
+    price = models.CharField('price', max_length=64)
+    image = models.CharField('image', max_length=128)
+    release_date = models.DateField('release_date')
+    lte_exists = models.BooleanField('lte_exists')
+    slug = models.CharField('slug', max_length=64)
+ 
--- databases/work_with_database/phones/views.py
+++ databases/work_with_database/phones/views.py
@@ -1,13 +1,39 @@
 from django.shortcuts import render
-
+from phones.models import Phone
+from django.http import HttpRequest
 
 def show_catalog(request):
+
     template = 'catalog.html'
     context = {}
+    context['data'] = []
+    sort_by = request.GET.get('sort')
+
+    phones = Phone.objects.all()
+
+    if sort_by == 'name':
+    	phones = Phone.objects.all().order_by('name')
+    elif sort_by == 'price_low':
+    	phones = Phone.objects.all().order_by('price')
+    elif sort_by == 'price_high':
+    	phones = Phone.objects.all().order_by('-price')
+
+
+    for phone in phones:
+
+    	context['data'].append({'name': phone.name, 'price': phone.price, 'image': phone.image, 
+    		'release_date': phone.release_date, 'lte_exists': phone.lte_exists, 'slug': phone.slug})
+
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
     context = {}
+
+    phone = Phone.objects.filter(slug=slug).get()
+
+    context['data'] = {'name': phone.name, 'price': phone.price, 'image': phone.image, 
+    		'release_date': phone.release_date, 'lte_exists': phone.lte_exists}
+
     return render(request, template, context)
